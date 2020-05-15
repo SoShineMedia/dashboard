0a52f6556c9f7f85a50f6610d0474de454d93df9
KenroyWhite <whitekenroy@yahoo.com>
2020-05-15 07:48:33 -0400

    new files

---
+++ src/js/components/common/Avatar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Img from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),      
+    },
+  },
+  center: {
+      textAlign:"center",
+      margin:theme.spacing(0),
+  },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+export default function Avatar() {
+  const classes = useStyles();
+
+  return (
+    <div>
+        <div className={classes.root}>
+            <Img alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.large}  />
+            <p>
+                <strong>Full Name</strong>
+            </p>
+        </div>
+        <p className={classes.center}>
+            <span>Lawyer</span>
+            <br/>
+            <span>Law Firm</span>
+        </p>
+    </div>
+    
+  );
+}

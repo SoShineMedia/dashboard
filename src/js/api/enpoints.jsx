aeb16426b23fc6ac2453bb7e4ec07d8c1c5be777
KenroyWhite <whitekenroy@yahoo.com>
2020-05-19 11:29:40 -0400

    add axios basics

---
+++ src/js/api/enpoints.jsx
@@ -0,0 +1,20 @@
+export const endpointV1 ={
+    
+    matter: {
+        list: {
+          all: {
+            link: '/v1/',
+            params: []
+          },
+          byUser: {
+            link: '/v1/',
+            params: []
+          }
+        },
+        detail: {
+          link: '/v1/',
+          params: []
+        }
+    },
+
+}
